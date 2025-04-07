8c6641460735016d3479e68c817ae0ab99f53eca
Deil <yar02030@gmail.com>
2025-04-07 17:17:29 +0700

    Системный вызов fork() часть 1

---
+++ pr004.c
@@ -0,0 +1,19 @@
+//Программа, порождающая дочерний процесс (часть 1)
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main() {
+	pid_t procid, pprocid;
+	int a=0, b;
+	procid = getpid();
+	pprocid = getppid();
+	printf("Ид. текущ. процесса: %d\nИд. род. процесса: %d\n", procid, pprocid);
+	printf("Начальное значение а: %d\n", a);
+	b = fork();
+	a = a+1;
+	procid = getpid();
+	pprocid = getppid();
+	printf("Ид. текущ. процесса: %d\nИд. род. процесса: %d\nНовое значение а: %d\n", procid, pprocid, a);
+	return 0;
+}	
