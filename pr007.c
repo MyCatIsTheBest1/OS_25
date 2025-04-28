d0e52501261a822697c207e2953caef826ea3deb
Deil <yar02030@gmail.com>
2025-04-28 16:10:25 +0700

    Программа измеяющая пользовательский контекст Версия 2

--- pr007.c
+++ pr007.c
@@ -5,9 +5,29 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[], char *envp[]) {
-	printf("Программа начала работу :...\n");
-	(void) execle("/bin/cat", "/bin/cat", "./pr007.c", 0, envp);
-	printf("Ошибка при выполнении системного вызова exe\n");
-	exit(-1);
+	printf("Программа начала работу ...\n");
+	pid_t pid, ppid;
+	int result;
+	pid = getpid();
+	ppid = getppid();
+	printf("ID процесса %d\n", pid);
+	printf("ID род. процесса %d\n", ppid);
+	result = fork();
+	if (result > 0) {
+		printf("Родительский процесс ...\n");
+		printf("ID процесса %d, ID процесса ребенка %d\n", pid, result);
+        	printf("ID род. процесса %d\n", ppid);
+		printf("Родитель закончил работу\n");
+	}
+	else if (result == 0) {
+		printf("Дочерний процесс ...\n");
+		(void) execle("./pr003.out", "./pr003.out", 0, envp); 
+		printf("Ошибка при выполнении системного вызова exec\n");
+		exit(-1);
+	}
+	else {
+		printf("Ошибка при выполнении системного вызова Fork\n");
+		exit(-1);
+	}	
 	return 0;
 }	
